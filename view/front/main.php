13e39fdd7cb9a49c888c0c92810bb238eab19a87
afourcad <fourcade.adri2@gmail.com>
2019-07-11 12:09:26 +0200

    changement flex

--- view/front/main.php
+++ view/front/main.php
@@ -13,9 +13,12 @@
 <?php ob_start(); ?>
 <main>
   <div class="leftVideoArea">
-    <video id="video"></video>
-    <canvas id="canvas"></canvas>
-    <button id="startbutton">Take photo</button>
+    <div id="camera" ondrop="drop(event)" ondragover="allowDrop(event)">
+      <canvas id="canva_filters" width="400px" height="300px"></canvas>
+      <video id="video" width="400px" height="300px" autoplay></video>
+      <canvas id="canvas" width="400px" height="300px"></canvas>
+      <!-- <img src="../../img/empty.png" style="width:100%;"> -->
+    </div>
   </div>
   <div class="rightGalerie">
   </div>
